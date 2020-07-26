f0fd2745e8eaf5c0d5c121787bacbb11011efad3
sonuahluwalia <sonuahluwalia@CORP.AMAZONWORKSPACES.COM>
2020-07-26 15:26:52 +0530

    sporty shoes first commit

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Requirements:
+1. Admin should be able to login/logout - done
+2. Admin should be able to change password
+3. Admin can add/delete/modify products
+4. Admin can change product categories
+5. Admin can view list of all users
+6. Admin can search users by name
+7. Admin can view purchase reports
+8. Admin can filter the purchase reports by date and category
---
+++ src/main/java/com/sporty/shoes/repositry/UserRepository.java
@@ -0,0 +1,17 @@
+package com.sporty.shoes.repositry;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.sporty.shoes.entity.User;
+ 
+@Transactional
+public interface UserRepository extends CrudRepository<User, Long> {
+ 
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
+    
+}
